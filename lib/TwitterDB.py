d2901fb772eea1d3f571eaa123acb707ba346808
guibla236 <70988109+guibla236@users.noreply.github.com>
2020-10-27 13:41:47 -0300

    Update TwitterDB.py
    New version. Improves code, documentation and functionality

--- lib/TwitterDB.py
+++ lib/TwitterDB.py
@@ -1,7 +1,26 @@
+import sqlite3
+from datetime import date
+
 class TwitterDataBase:
-    def __init__(self,version, date):
-        self.fileName=version
-        self.tableName=date
+    def __init__(self, date,cols_names):
+        """
+        This class creates the database object to use with Twitter Sniffer. 
+        The name of the database saved in the path directory is the actual date. 
+        When a new day is detected, the DB file changes automatically 
+        (when a new row is attached).
+        Thanks to this, the only needed parameter to initialize this object
+        is the column names that the DB is going to have.
+        
+        -cols_names is a list of strings who has the cols names desired.
+        
+        This class has the method insertData. Use this to insert data respecting
+        amount of columns defined in this initialization.
+        """
+        self.fileName=date
+        self.tableName="Table1"
+        self.setColsNames(",".join(cols_names))
+        self.baseCreator(self.getColsNames())
+        
 
     def getTableName(self):
         return(self.tableName)
@@ -15,29 +34,29 @@ class TwitterDataBase:
     def setFileName(self,newFileName):
         self.fileName=newFileName
 
-    def setColsName(self,cols):
+    def setColsNames(self,cols):
         self.cols=cols
 
-    def getColsName(self):
+    def getColsNames(self):
         return(self.cols)
 
-    def baseCreator(self,cols=None):
-        """Nombre del archivo ".db", nombre de la tabla a buscar
-        y nombre de las columnas separadas por coma y entrecomilla,
-        ejemplo: '(var1,var2,...)' """
-        import sqlite3
-        self.conector=sqlite3.connect(self.getFileName()+'.db')
+    def baseCreator(self,newFileName=None,cols=None):
+        
+        if newFileName:
+            self.conector=sqlite3.connect(newFileName+'.db')
+            self.setFileName(newFileName)
+        
+        else:
+            self.conector=sqlite3.connect(self.getFileName()+'.db')
+
         self.cursor=self.conector.cursor()
-        if cols!=None:
-            self.tableCreator(self.getTableName(),cols)
-
-    def tableCreator(self,tableName,cols=None):
-        if tableName!=self.getTableName():
+        self.tableCreator(self.getTableName(),self.getColsNames())
+        
+    
+    def tableCreator(self,tableName):
+        if tableName!=self.getTableName(): 
             self.setTableName(tableName)
-
-        if cols!=None:
-            self.setColsName(cols)
-
+       
         self.getCursor().execute("CREATE TABLE IF NOT EXISTS "+self.getTableName()+"("+self.getColsName()+")")
 
 
@@ -52,16 +71,24 @@ class TwitterDataBase:
 
     def closeConector(self):
         self.conector.close()
-    def insertData(self,col1,col2,col3,col4,col5,col6,col7,col8):
-        from datetime import date
-        if date.today().strftime("_%d_%m_%Y")!=self.getTableName():
-            print("CAMBIO DE DÍA")
-            self.tableCreator(date.today().strftime("_%d_%m_%Y"))
-
-        #DEBUG: else:
-            #Implementar
-            #print("Mismo día")
-
-        self.getCursor().execute("INSERT INTO {} VALUES (?,?,?,?,?,?,?,?)".format(self.getTableName()),
-                    (col1,col2,col3,col4,col5,col6,col7,col8))
+    
+    def insertData(self,cols):
+        """
+        
+        Insert the twit data into the SQLite database.
+        
+        -cols is a list ordered in the same way that the DB is defined. In example: element 1 of the list is assigned to column 1 of the DB.
+        
+        If an error occurs, probably there are an error in the number of elements which is passed or the type of element assigned.
+        """
+        if date.today().strftime("%d_%m_%Y")!=self.getFileName():
+            self.closeCursor()
+            self.closeConector()
+            self.baseCreator(date.today().strftime("%d_%m_%Y"))
+        
+        data=",".join(cols)
+        
+        self.getCursor().execute("INSERT INTO {} VALUES (?)".format(self.getTableName()),
+                    (data))
+        
         self.getConector().commit()
